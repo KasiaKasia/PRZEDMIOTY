489aa22f562f66734dea61e214f67bd04f3a1f34
--add <bulicka.alexandra@gmail.com>
2025-09-06 21:34:11 +0200

    [refactor] - rename files

---
+++ ELEMENTY_PROGRAMOWANIA/1I-gr_2/zad1/zad1.cpp
@@ -0,0 +1,17 @@
+﻿ 
+#include <iostream>
+using namespace std;
+int main()
+{
+     cout << "Hello World!\n";
+
+
+    int wiek;
+     
+     cout << "Podaj swoj wiek: ";
+     
+    cin >> wiek;
+     
+    cout << "Masz " << wiek << " lat." << std::endl;
+}
+ 
