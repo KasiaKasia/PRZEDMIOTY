eb9df21f1565b4e9ecdc8d32ae020d2545da59a8
--add <bulicka.alexandra@gmail.com>
2025-09-19 00:32:09 +0200

    [feat] - Elementy programowania

---
+++ ELEMENTY_PROGRAMOWANIA/1I-gr_1/zadania-lekcja-3/Inkrementacja_Dekrementacja/Inkrementacja_Dekrementacja.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+
+    cout << "PREINkrementacja: " << ++a << endl;
+    cout << "POSTINkrementacja: " << a++ << endl;
+    cout << "Wartość po: " << a << endl;
+
+    int b = 5;
+    cout << "PREDEkrementacja: " << --b << endl; 
+    cout << "POSTDEkrementacja: " << b-- << endl;
+    cout << "Wartość po: " << b << endl;
+
+    return 0;
+}
