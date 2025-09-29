47dce9078e5db4b11bb8db4677dea49bc69de2ef
--add <bulicka.alexandra@gmail.com>
2025-09-29 03:51:35 +0200

    [feat] - Podstawy programowania

---
+++ PODSTAWY_PROGRAMOWANIA/2A-gr1/zadania-lekcja-8_struktury_unie/struktury_unie/struktura_zagniezdzona.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+struct Adres {
+    std::string miasto;
+    std::string ulica;
+    int numerDomu;
+};
+
+struct Osoba {
+    std::string imie;
+    int wiek;
+    Adres adres;  
+};
+
+int main() {
+    Osoba anna;
+    anna.imie = "Anna";
+    anna.wiek = 25;
+    anna.adres.miasto = "Warszawa";   
+    anna.adres.ulica = "Marszałkowska";
+    anna.adres.numerDomu = 10;
+
+    std::cout << anna.imie << " mieszka w " << anna.adres.miasto << std::endl;
+    return 0;
+}
