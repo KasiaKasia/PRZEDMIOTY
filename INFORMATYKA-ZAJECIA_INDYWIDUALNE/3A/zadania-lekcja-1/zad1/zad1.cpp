63d44c7a135d148e132ab3fbad4180cccd5eb5a9
--add <bulicka.alexandra@gmail.com>
2025-10-07 04:27:27 +0200

    [feat] - Informatyka - zajęcia indywidualne

---
+++ INFORMATYKA-ZAJECIA_INDYWIDUALNE/3A/zadania-lekcja-1/zad1/zad1.cpp
@@ -0,0 +1,17 @@
+ 
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+
+    int wiek;
+     
+    std::cout << "Podaj swoj wiek: ";
+     
+    std::cin >> wiek;
+     
+    std::cout << "Masz " << wiek << " lat." << std::endl;
+}
+ 
