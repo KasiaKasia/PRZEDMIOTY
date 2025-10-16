ec91104645cc0b6d287de11c625bc6bad24259f1
--add <bulicka.alexandra@gmail.com>
2025-10-16 06:41:01 +0200

    [feat] - podstawy programowania

---
+++ PODSTAWY_PROGRAMOWANIA/2A-gr1/zadania-lekcja-10_Usystematyzowanie_materialu/Zadanie-2-Funkcja_rekurencyjna/zad-2-funkcja-rekurencyjna.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+long long factorial(int n) {
+    if (n <= 1)
+        return 1; // warunek bazowy
+    else
+        return n * factorial(n - 1); // wywołanie rekurencyjne
+}
+
+int main() {
+    int n;
+    cout << "Podaj liczbe nieujemna: ";
+
+     
+
+    return 0;
+}
+
+/*
+Zadanie 2 – Funkcja rekurencyjna
+
+Treść :
+Napisz program w C++, który oblicza silnię liczby n wprowadzanej przez użytkownika.
+
+Wykorzystaj funkcję rekurencyjną. Program powinien sprawdzić, czy wprowadzona liczba jest nieujemna.
+*/
