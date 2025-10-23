9aaf734256d8ffc92011293f99d13edf23b1f3f9
--add <bulicka.alexandra@gmail.com>
2025-10-23 02:12:14 +0200

    [feat] - Podstawy programowania

---
+++ PODSTAWY_PROGRAMOWANIA/2A-gr1/zadania-lekcja-11-Dziedziczenie-nadpisywanie_metod/zadania-do-rozwiazania/zad-2-Nadpisywanie_metod/zad-2-Nadpisywanie_metod.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+class Pojazd { 
+public:
+   
+};
+
+class Rower : public Pojazd {
+public:
+    
+};
+
+int main() {
+
+    return 0;
+}
+
+/*
+Zadanie 2: Nadpisywanie metod
+
+Opis zadania:
+Stwórz klasę bazową Pojazd z wirtualną metodą jedz(), która wyświetla ogólny komunikat. 
+Następnie utwórz klasę pochodną Rower dziedziczącą z Pojazd i nadpisz metodę jedz(), aby wyświetlała specyficzny komunikat dla roweru. 
+
+W funkcji main() użyj polimorfizmu: utwórz wskaźnik do Pojazd wskazujący na obiekt Rower i wywołaj metodę jedz().
+*/
