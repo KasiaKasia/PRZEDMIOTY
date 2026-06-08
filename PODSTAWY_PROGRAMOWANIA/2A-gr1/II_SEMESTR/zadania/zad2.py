08499d7b43487ba5ea2e95487d3af32d621231d6
KasiaKasia <bulicka.alexandra@gmail.com>
2026-06-08 03:49:39 +0200

    [feat] - Podstawy programowania

---
+++ PODSTAWY_PROGRAMOWANIA/2A-gr1/II_SEMESTR/zadania/zad2.py
@@ -0,0 +1,206 @@
+''' Zadanie 1
+
+Utwórz zmienną imie z własnym imieniem i wyświetl komunikat:
+
+Cześć, Anna!
+
+używając konkatenacji (+).
+'''
+ 
+
+
+
+
+''' Zadanie 2
+
+Utwórz zmienne:
+
+imie = "Anna"
+wiek = 25
+
+i wyświetl:
+Mam na imię Anna i mam 25 lat.
+
+za pomocą f-stringa.
+'''
+
+
+''' Zadanie 3
+
+Dla napisu:
+
+tekst = "Python"
+
+wyświetl:
+wszystkie litery wielkie,
+wszystkie litery małe.
+'''
+
+
+'''Zadanie 4
+
+Dla napisu:
+
+tekst = "programowanie"
+
+wyświetl pierwsze 7 znaków.
+
+Wynik:
+program
+'''
+
+
+
+
+
+
+'''Zadanie 5
+
+Dla napisu:
+
+tekst = "Python jest super"
+
+wyświetl napis od końca.
+'''
+
+
+
+
+
+'''Zadanie 6
+
+Powiel napis:
+
+tekst = "ha"
+
+trzykrotnie.
+
+Wynik:
+hahaha
+'''
+
+
+# split()
+
+'''Zadanie 7
+Podziel zdanie:
+
+zdanie = "Ala ma kota"
+
+na listę słów.
+
+Oczekiwany wynik:
+
+['Ala', 'ma', 'kota']
+
+'''
+
+
+
+'''Zadanie 8
+
+Podziel datę:
+
+data = "2025-12-24"
+
+na rok, miesiąc i dzień.
+'''
+
+
+'''Zadanie 9
+
+Dla danych:
+
+dane = "jan:kowalski"
+
+wyświetl:
+Imię: jan
+Nazwisko: kowalski
+'''
+
+'''Zadanie 10
+
+Podziel napis:
+
+produkty = "mleko,chleb,jajka,masło"
+
+na listę produktów. 
+Oczekiwany wynik:
+['mleko', 'chleb', 'jajka', 'masło']
+
+'''
+
+
+# join()
+
+'''Zadanie 11
+
+Połącz listę:
+
+kolory = ["czerwony", "zielony", "niebieski"]
+
+tak, aby otrzymać:
+czerwony, zielony, niebieski
+'''
+
+
+'''Zadanie 12
+
+Połącz słowa:
+
+slowa = ["Python", "jest", "łatwy"]
+
+znakiem -.
+
+Wynik:
+Python-jest-łatwy
+'''
+
+'''Zadanie 13
+
+Z listy:
+
+cyfry = ["1", "2", "3", "4"]
+
+utwórz napis:
+1234
+
+'''
+
+# replace()
+
+'''Zadanie 14
+
+Zamień w napisie:
+
+tekst = "Lubię koty"
+
+słowo koty na psy.
+
+'''
+
+'''Zadanie 15
+
+Usuń wszystkie wykrzykniki z napisu:
+
+tekst = "Python!!!"
+
+'''
+
+
+
+'''Zadanie 16
+
+Dla numeru telefonu:
+
+telefon = "+48 123-456-789"
+
+usuń:
+
+prefiks +48
+wszystkie myślniki
+
+Wynik:
+123456789
+
+'''
