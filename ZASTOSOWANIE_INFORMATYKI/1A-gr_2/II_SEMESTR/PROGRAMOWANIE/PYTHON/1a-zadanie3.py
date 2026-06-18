f17d3d05183f4db9cf0ed9a448415b745edc0aab
KasiaKasia <bulicka.alexandra@gmail.com>
2026-06-18 03:17:15 +0200

    [feat] - Zastosowanie informatyki

---
+++ ZASTOSOWANIE_INFORMATYKI/1A-gr_2/II_SEMESTR/PROGRAMOWANIE/PYTHON/1a-zadanie3.py
@@ -0,0 +1,48 @@
+'''
+Zadanie 1: Klasa Uczen
+
+Napisz klasę Uczen, która będzie przechowywać podstawowe informacje o uczniu.
+
+Wymagania:
+1. Klasa powinna posiadać następujące właściwości:
+    imie
+    nazwisko
+2. Utwórz konstruktor (__init__), który będzie przyjmował imię i nazwisko podczas tworzenia obiektu.
+3. Dodaj metodę wyswietl_informacje(), która wyświetli dane ucznia w formacie:
+    Imię: Jan
+    Nazwisko: Kowalski
+4. Utwórz obiekt klasy dla ucznia o imieniu Jan i nazwisku Kowalski.
+5. Wywołaj metodę wyswietl_informacje().
+
+Przykładowy wynik programu:
+Imię: Jan
+Nazwisko: Kowalski
+
+'''
+'''
+Zadanie 2: Klasa Produkt
+
+Napisz program w języku Python, który będzie przechowywał informacje o produktach.
+
+Wymagania:
+1. Utwórz klasę Produkt.
+2. Klasa powinna posiadać następujące właściwości:
+    nazwa
+    typ
+    cena
+3. Zaimplementuj konstruktor (__init__), który pozwoli ustawić wszystkie właściwości podczas tworzenia obiektu.
+4. Dodaj metodę wyswietl_informacje(), która wyświetli informacje o produkcie w następującym formacie:
+    Nazwa: Laptop
+    Typ: Elektronika
+    Cena: 3500 zł
+5. Utwórz obiekt reprezentujący produkt:
+    nazwa: Laptop
+    typ: Elektronika
+    cena: 3500
+6.  Wywołaj metodę wyswietl_informacje().
+
+Oczekiwany wynik:
+Nazwa: Laptop
+Typ: Elektronika
+Cena: 3500 zł
+'''
