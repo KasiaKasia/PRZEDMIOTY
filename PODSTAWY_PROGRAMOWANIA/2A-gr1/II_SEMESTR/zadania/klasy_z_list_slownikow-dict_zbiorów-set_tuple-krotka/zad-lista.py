e76017ebb848a755b2c74b57537b7ce5f1db8f9a
KasiaKasia <bulicka.alexandra@gmail.com>
2026-06-18 03:12:40 +0200

    [feat] - Podstawy programowania

---
+++ PODSTAWY_PROGRAMOWANIA/2A-gr1/II_SEMESTR/zadania/klasy_z_list_slownikow-dict_zbiorów-set_tuple-krotka/zad-lista.py
@@ -0,0 +1,30 @@
+'''
+Zadanie: Lista obecności uczniów
+
+Napisz klasę ListaObecnosci, która będzie przechowywać nazwiska uczniów w liście.
+
+Klasa powinna umożliwiać:
+
+   - dodawanie ucznia do listy,
+   - usuwanie ucznia z listy,
+   - wyświetlanie wszystkich uczniów znajdujących się na liście,
+   - wyszukiwanie pozycji (indeksu) ucznia na liście,
+   - zmianę nazwiska ucznia na inne.
+Wymagane metody
+    - dodaj(nazwisko) – dodaje ucznia do listy,
+    - usun(nazwisko) – usuwa ucznia z listy,
+    -wyswietl() – wyświetla całą listę uczniów,
+    - szukaj(nazwisko) – zwraca indeks ucznia,
+    - edytuj(stare_nazwisko, nowe_nazwisko) – zmienia nazwisko ucznia.
+
+Przykładowe działania programu
+- Dodaj uczniów:
+    Kowalski
+    Nowak
+    Wiśniewski
+- Wyświetl listę uczniów.
+- Znajdź indeks ucznia „Nowak”.
+- Zmień nazwisko „Nowak” na „Kaczmarek”.
+- Usuń ucznia „Wiśniewski”.
+- Ponownie wyświetl listę uczniów.
+'''
---
+++ PODSTAWY_PROGRAMOWANIA/2A-gr1/II_SEMESTR/zadania/klasy_z_list_slownikow-dict_zbiorów-set_tuple-krotka/zad-slownik.py
@@ -0,0 +1,61 @@
+'''
+Zadanie
+Klasa zarządzająca książkami
+
+Treść zadania:
+
+Napisz klasę Biblioteka, która będzie przechowywać informacje o książkach w słowniku.
+
+Klasa powinna umożliwiać:
+
+   - dodawanie książki (tytuł → autor),
+   - usuwanie książki po tytule,
+   - wyświetlanie całego słownika książek,
+   - wyszukiwanie autora po tytule książki,
+   - edycję autora istniejącej książki.
+
+Wymagania
+
+Utwórz klasę Biblioteka z metodami:
+______________________________________________
+Metoda                      |   Opis
+dodaj(tytul, autor)	        |   Dodaje książkę do słownika.
+usun(tytul)                 |	Usuwa książkę o podanym tytule.
+wyswietl()	                |   Zwraca lub wyświetla wszystkie książki.
+szukaj(tytul)		        |   Zwraca autora danej książki.
+edytuj(tytul, nowy_autor)	|   Zmienia autora wskazanej książki.
+
+ 
+
+Przykładowe użycie programu
+
+Program powinien umożliwiać wykonanie operacji takich jak:
+- dodanie książki „Lalka” → „Bolesław Prus”,
+- dodanie książki „Quo Vadis” → „Henryk Sienkiewicz”,
+- wyświetlenie wszystkich książek,
+- wyszukanie autora książki „Lalka”,
+- zmiana autora (edycja danych),
+- usunięcie wybranej książki.
+
+'''
+
+''' 
+class Biblioteka:
+    def __init__(self):
+        self.ksiazki = {}
+
+    def dodaj(self, tytul, autor): 
+
+    def usun(self, tytul): 
+
+    def wyswietl(self): 
+
+    def szukaj(self, tytul): 
+
+    def edytuj(self, tytul, nowy_autor):
+'''  
+
+      
+    
+
+ 
