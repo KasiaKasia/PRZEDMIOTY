305711a05ac21b913f06644897dbba84dea7f34a
KasiaKasia <bulicka.alexandra@gmail.com>
2026-09-17 02:43:13 +0200

    [refactor] - refactor

--- 2026-2027/PROGRAMOWANIE_APLIKACJI_INTERNETOWYCH-teoria/5A/JAVASCRIPT_TYPESCRIPT/use_strict.js
+++ 2026-2027/PROGRAMOWANIE_APLIKACJI_INTERNETOWYCH-teoria/5A/JAVASCRIPT_TYPESCRIPT/use_strict.js
@@ -1,16 +1,16 @@
 
- 
+"use strict" 
 // 1.  
 // Bez "use strict" działa:
 // W trybie "use strict" : ReferenceError: message2 is not defined
-// message2 = "Witaj z TypeScript bez 'use strict'!"; 
+let message2 = "Witaj z TypeScript bez 'use strict'!"; 
 
 // 2. 
 // Bez "use strict" this wskazuje na obiekt globalny, np. window.
 // W trybie "use strict" this jest undefined w funkcjach 
-// function show() {
-//     console.log(this);
-// }
+function show() {
+    console.log(this);
+}
 
 // show(); // undefined
 
@@ -28,13 +28,13 @@ person.name = "Adam"; // W trybie "use strict" : TypeError: Cannot assign to rea
 console.log(person.name); 
 */
 // 4. 
-/* 
+ 
 function add(a, a) {
     console.log(a); // W trybie "use strict" : SyntaxError: Duplicate parameter name not allowed in this context
     return a;
 }
 add(2,6); // W trybie "use strict" : SyntaxError: Duplicate parameter name not allowed in this context
-*/ 
+ 
 // 5.
  
 // let osoba = {
